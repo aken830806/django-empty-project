62df4f819ee4e9db5f7acb07190d3d15e483521a
njzhan <t07087@mail.fcu.edu.tw>
2020-11-26 13:42:16 +0800

    新增 local_settings_example、修改 wsgi.py、統一檔案結尾(LF)

--- EmptyProject/wsgi.py
+++ EmptyProject/wsgi.py
@@ -1,21 +1,12 @@
-"""
-WSGI config for EmptyProject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
 import os
-os.environ["DJANGO_SETTINGS_MODULE"] = "EmptyProject.settings"
-
 import sys
 from os.path import dirname, abspath
+from django.core.wsgi import get_wsgi_application
+
+from EmptyProject.settings import VIRTUALENV_PATH
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "EmptyProject.settings"
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
 sys.path.insert(0, PROJECT_DIR)
-from EmptyProject.settings import VIRTUALENV_PATH
 sys.path.append(VIRTUALENV_PATH)
-
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
