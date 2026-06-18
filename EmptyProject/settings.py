6f96a622d0739d08c51733fd3f86527eb5198d1f
Chen chen <43808882+bitifyChen@users.noreply.github.com>
2026-06-18 14:19:07 +0800

    Upgrade Django to 5.2 LTS

--- EmptyProject/settings.py
+++ EmptyProject/settings.py
@@ -110,8 +110,6 @@ TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
-USE_L10N = True
-
 USE_TZ = True
 
 
@@ -122,7 +120,14 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
 ]
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage',
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
